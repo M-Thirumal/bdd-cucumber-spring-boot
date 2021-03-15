8e8c997931009179272c07e546b2435f8d112c73
Thirumal <m.thirumal@hotmail.com>
2021-03-15 11:00:39 +0530

    first commit

---
+++ src/main/java/in/thirumal/BddCucumberSoringBootApplication.java
@@ -0,0 +1,13 @@
+package in.thirumal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BddCucumberSoringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BddCucumberSoringBootApplication.class, args);
+	}
+
+}
